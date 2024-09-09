4b164d9a59ea4954efeabf2c5b3d647da8486f66
Harley Thomas <harleyjackthomas@gmail.com>
2024-09-09 09:53:43 +0930

    Progressing openapi annotations

--- src/external-resolver/external-resolver.controller.ts
+++ src/external-resolver/external-resolver.controller.ts
@@ -8,6 +8,7 @@ import {
   Put,
   Query,
 } from "@nestjs/common";
+import { ApiOperation } from "@nestjs/swagger";
 import { PaginationDto } from "src/shared/dto";
 import { UpsertExternalResolverDto } from "./external-resolver.dto";
 import { ExternalResolverService } from "./external-resolver.service";
@@ -18,26 +19,41 @@ export class ExternalResolveController {
     private readonly externalResolverService: ExternalResolverService,
   ) {}
 
+  @ApiOperation({
+    operationId: "createExternalResolver",
+  })
   @Post()
   async create(@Body() dto: UpsertExternalResolverDto) {
     return this.externalResolverService.upsertExternalResolverSet(dto);
   }
 
+  @ApiOperation({
+    operationId: "updateExternalResolver",
+  })
   @Put()
   async update(@Body() dto: UpsertExternalResolverDto) {
     return this.externalResolverService.upsertExternalResolverSet(dto);
   }
 
+  @ApiOperation({
+    operationId: "getExternalResolver",
+  })
   @Get(":id")
   async get(@Param("id") id: string) {
     return this.externalResolverService.get(id);
   }
 
+  @ApiOperation({
+    operationId: "getManyExternalResolvers",
+  })
   @Get()
   async getMany(@Query() paginationDto: PaginationDto) {
     return this.externalResolverService.getMany(paginationDto);
   }
 
+  @ApiOperation({
+    operationId: "deleteExternalResolver",
+  })
   @Delete(":id")
   async delete(@Param("id") id: string) {
     return this.externalResolverService.delete(id);
--- src/object/object.controller.ts
+++ src/object/object.controller.ts
@@ -9,15 +9,29 @@ import {
   Req,
   Res,
 } from "@nestjs/common";
+import { ApiOperation, ApiResponse } from "@nestjs/swagger";
 import { Request, Response } from "express";
 import { PaginationDto } from "src/shared/dto";
-import { CreateObjectDto, CreateObjectResponseDto } from "./object.dto";
+import {
+  CreateObjectDto,
+  CreateObjectResponseDto,
+  HermesObject,
+} from "./object.dto";
 import { ObjectService } from "./object.service";
 
 @Controller("objects")
 export class ObjectController {
   constructor(private readonly objectService: ObjectService) {}
 
+  @ApiOperation({
+    operationId: "createObject",
+    summary: "Create a new Object",
+  })
+  @ApiResponse({
+    status: 201,
+    description: "The key and presigned URL of the Object.",
+    type: CreateObjectResponseDto,
+  })
   @Post()
   async resolve(
     @Req() request: Request<CreateObjectDto>,
@@ -35,6 +49,15 @@ export class ObjectController {
     }
   }
 
+  @ApiOperation({
+    operationId: "getObject",
+    summary: "Get an Object by ID",
+  })
+  @ApiResponse({
+    status: 200,
+    description: "The Object was found and returned.",
+    type: HermesObject,
+  })
   @Get(":id")
   async get(@Param("id") id: string) {
     const object = await this.objectService.get(id);
@@ -42,11 +65,24 @@ export class ObjectController {
     return object;
   }
 
+  @ApiOperation({
+    operationId: "getObjects",
+    summary: "Get all Objects",
+  })
+  @ApiResponse({
+    status: 200,
+    description: "The Objects were found and returned.",
+    type: [HermesObject],
+  })
   @Get()
   async getMany(@Query() paginationDto: PaginationDto) {
     return this.objectService.getMany(paginationDto);
   }
 
+  @ApiOperation({
+    operationId: "deleteObject",
+    summary: "Delete an Object by ID",
+  })
   @Delete(":id")
   async remove(@Param("id") id: string) {
     return this.objectService.delete(id);
