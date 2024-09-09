e442fd812f6b7d0765eb6e683bcff8dfb96b3008
Harley Thomas <38710304+harleyjackthomas@users.noreply.github.com>
2024-09-09 10:28:48 +0930

    Updating required properties for openapi types (#14)

--- src/external-resolver/external-resolver.dto.ts
+++ src/external-resolver/external-resolver.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Type } from "class-transformer";
 import {
   IsArray,
@@ -8,18 +9,39 @@ import {
 } from "class-validator";
 
 export class UpsertExternalResolverDto {
+  @ApiProperty({
+    required: false,
+    description:
+      "The ID of the External Resolver. This causes an UPDATE, not a CREATE.",
+  })
   @IsString()
   id?: string;
 
+  @ApiProperty({
+    required: true,
+    description: "The href of the External Resolver.",
+  })
   @IsString()
   href: string;
 
+  @ApiProperty({
+    required: true,
+    description: "The pattern of the External Resolver.",
+  })
   @IsString()
   pattern: string;
 
+  @ApiProperty({
+    required: true,
+    description: "The qualifier of the External Resolver.",
+  })
   @IsString()
   qualifier: string;
 
+  @ApiProperty({
+    required: true,
+    description: "The creation data of the External Resolver.",
+  })
   @IsArray()
   @Length(1)
   @ValidateNested({ each: true })
@@ -28,24 +50,53 @@ export class UpsertExternalResolverDto {
 }
 
 export class ExternalResolverDto {
+  @ApiProperty({
+    required: true,
+    description: "The ID of the External Resolver.",
+  })
   @IsString()
   id?: string;
 
+  @ApiProperty({
+    required: true,
+    description: "The qualifier of the External Resolver.",
+  })
   @IsString()
   qualifier: string;
 
+  @ApiProperty({
+    required: true,
+    description: "The href of the External Resolver.",
+  })
   @IsString()
   href: string;
 
+  @ApiProperty({
+    required: true,
+    description: "The pattern of the External Resolver.",
+  })
   @IsString()
   pattern: string;
 
+  @ApiProperty({
+    required: true,
+    description: "The creation date of the External Resolver.",
+  })
   @IsDate()
   createdAt: Date;
 
+  @ApiProperty({
+    required: true,
+    description: "The last update date of the External Resolver.",
+  })
   @IsDate()
   updatedAt: Date;
 
+  @ApiProperty({
+    required: true,
+    description: "The child External Resolvers of the External Resolver.",
+    type: () => [ExternalResolverDto],
+  })
   @IsArray()
   @Length(1)
   @ValidateNested({ each: true })
