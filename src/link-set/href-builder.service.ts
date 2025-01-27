e7f0041faa41af088cfb9ce813fca692bb84c26c
Harley Thomas <105835658+harleytrustprovenance@users.noreply.github.com>
2025-01-28 10:22:18 +1030

    Adding injectable href builder service. (#18)
    Co-authored-by: Harley Thomas <harleyjackthomas@gmail.com>

---
+++ src/link-set/href-builder.service.ts
@@ -0,0 +1,28 @@
+import { Inject, Injectable } from "@nestjs/common";
+import { PrismaClient } from "@prisma/client";
+import * as DataLoader from "dataloader";
+
+export interface HrefBuilderService {
+  buildHref(id: string): Promise<string>;
+}
+
+@Injectable()
+export class HrefBuilderService implements HrefBuilderService {
+  private loader = new DataLoader(async (ids: string[]) => {
+    return this.loaderFunction(ids);
+  });
+
+  constructor(@Inject("PRISMA_CLIENT") private readonly prisma: PrismaClient) {}
+
+  async buildHref(id: string): Promise<string | undefined> {
+    return this.loader.load(id);
+  }
+
+  private async loaderFunction(ids: string[]): Promise<string[]> {
+    const links = await this.prisma.link.findMany({
+      where: { id: { in: ids } },
+    });
+
+    return ids?.map((id) => links.find((link) => link.id === id)?.href);
+  }
+}
--- src/link-set/link-set.module.ts
+++ src/link-set/link-set.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
 import { PrismaModule } from "../prisma/prisma.module";
+import { HrefBuilderService } from "./href-builder.service";
 import { LinkSetController } from "./link-set.controller";
 import { LinkSetService } from "./link-set.service";
 
 @Module({
   imports: [PrismaModule],
   controllers: [LinkSetController],
-  providers: [LinkSetService],
+  providers: [LinkSetService, HrefBuilderService],
 })
 export class LinkSetModule {}
